9a508bd72820625a0bddc8763ce91697f6963b41
sun <863@qq.com>
2019-05-02 20:11:15 +0800

    提交

--- src/test/java/com/itcast/controller/CRUDTest.java
+++ src/test/java/com/itcast/controller/CRUDTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
+
 public class CRUDTest {
     @Autowired
     private UserService userService;
